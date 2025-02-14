110d445edecdc373ee424689439152e1767830be
sara_m <sara.miromano@gmail.com>
2025-02-14 17:12:15 -0500

    Click functions added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 // console.log(button);
 
-button.addEventListener("mouseover", function () {
-    button.innerText = "Its Working!"
-    cat.classList.add("show");
+button.addEventListener("click", function () {
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!"
+        button.classList.add("disappear")
+        // console.log("Yes")
+        
+    }
+    else  {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!"
+        button.classList.remove("disappear");
+        // console.log("No")
+        
+    }
+
 });
